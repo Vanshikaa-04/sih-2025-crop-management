f3edb3815ac4b0f9dc5e338d8a0140f81cff5136
Vanshikaa-04 <vanshikayadav.817@gmail.com>
2025-12-16 17:11:58 +0530

    feat: integrate Tailwind CSS and update App layout with Header and Footer components

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -88,3 +88,4 @@ export const Header = () => {
         </header>
     );
 };
+export default Header;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: '/sih-2025-crop-management/'
 })
